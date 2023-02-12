c9b807d0c7ac59fdc36d92950c9dbeb1abc0510c
mmm1602 <migue.ahg16@gmail.com>
2023-02-12 18:14:30 -0500

    added collition with paddles and paddles movement

--- js/gameController.js
+++ js/gameController.js
@@ -10,15 +10,16 @@
     let ballRadius = 10;
     let x = canvas.width / 2;
     let y = (canvas.height /2) - ballRadius;
-    let dx = 1;
-    let dy = -1;
+    let dx = 2;
+    let dy = -2;
 
 
     //Paddle variables
     let paddleHeight = 150;
     let paddleWidth = 20;
-    let paddleX = (canvas.width - paddleWidth) / 2;
-    let paddleY = (canvas.height / 2) - paddleHeight;
+    let paddle0_Y = (canvas.height / 2) - paddleHeight;
+    let paddle1_Y = (canvas.height / 2) - paddleHeight;
+
 
     function drawBall() {
         ctx.beginPath();  
@@ -28,9 +29,9 @@
         ctx.closePath();
     }
 
-    function drawPaddle(posX) {
+    function drawPaddle(posX, posY) {
         ctx.beginPath();
-        ctx.rect(posX, paddleY, paddleWidth, paddleHeight);
+        ctx.rect(posX, posY, paddleWidth, paddleHeight);
         ctx.fillStyle = "#0095DD";
         ctx.fill();
         ctx.closePath();
@@ -39,11 +40,35 @@
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
-        drawPaddle(canvas.width - 100);
-        drawPaddle(100);
+        drawPaddle(canvas.width - 100, paddle0_Y);
+        drawPaddle(100, paddle1_Y);
+
+        window.addEventListener("keydown", (e) => {
+            if(e.key == "ArrowUp"){
+                paddle0_Y -= 0.05;
+            }
+            else if(e.key == "ArrowDown"){
+                paddle0_Y += 0.05;
+            }
+        })
+        window.addEventListener("keydown", (e) => {
+            if(e.key == "w"){
+                paddle1_Y -= 0.05;
+            }
+            else if(e.key == "s"){
+                paddle1_Y += 0.05;
+            }
+        })
 
         if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
             dy = -dy;
+        }
+        else if (x > (canvas.width -100) -paddleWidth/2 || x < 100 + paddleWidth) {
+            dy = -dy;
+            dx = -dx;
+        } 
+        else if(x - dx < 100 + (paddleWidth)){
+            dx = --dx;
         }
         x += dx;
         y += dy;
